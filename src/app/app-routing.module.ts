85b09f2a0503ea033a0ef3456d1a5d5f6042d6cc
GoorezyEST <goorezyestudios@gmail.com>
2022-09-22 18:18:42 -0300

    Created gallery first projects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ExperienceComponent } from './experience/experience.component';
+import { GalleryComponent } from './gallery/gallery.component';
 import { HeaderComponent } from './header/header.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 const routes: Routes = [
   { path: '', component: HeaderComponent, pathMatch: 'full' },
   { path: 'experience', component: ExperienceComponent },
+  { path: 'experience/gallery', component: GalleryComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { ThreeModule } from './experience/three.module';
 import { HeaderComponent } from './header/header.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, NotFoundComponent],
+  declarations: [AppComponent, HeaderComponent, NotFoundComponent, GalleryComponent],
   imports: [BrowserModule, AppRouting, ThreeModule],
   providers: [],
   bootstrap: [AppComponent],

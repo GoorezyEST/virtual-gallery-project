d27985142df05257ff4d3b53f64c3b64fae88622
GoorezyEST <goorezyestudios@gmail.com>
2022-09-24 16:48:46 -0300

    Add responsive adapt to every layout except the cube

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,4 +9,20 @@ export class HeaderComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  toAbout(el: HTMLElement, linkOne: HTMLElement, linkTwo: HTMLElement) {
+    el.classList.remove('hidden');
+    linkOne.classList.remove('active-link');
+    linkTwo.classList.add('active-link');
+  }
+
+  toHome(el: HTMLElement, linkOne: HTMLElement, linkTwo: HTMLElement) {
+    if (el.classList.contains('hidden')) {
+      return;
+    } else {
+      el.classList.add('hidden');
+      linkTwo.classList.remove('active-link');
+      linkOne.classList.add('active-link');
+    }
+  }
 }
